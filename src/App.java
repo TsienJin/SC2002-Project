e7fdcbd5318599f8ba9a0ea0b65f8249e43d7986
Tsien Jin <tsien.jin@gmail.com>
2022-10-20 13:04:09 +0800

    testing

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import dataDriver.FileIO;
 
@@ -12,8 +13,9 @@ public class App{
         // reader.writeToFile("test.csv", "WritingTest");
         // ArrayList<String> res = reader.readFromFile("movie.csv");
         // System.out.println(reader.findMatchFromFile("movie.csv", "1375666"));
-        String toUpdate = "0111161,The Shawshank Redemption,1,Drama,Too twoo Three long,Bob";
-        FileIOObj.updateInFile("movie.csv", toUpdate);
-        FileIOObj.deleteKeyInFile("movie.csv", "1375666");
+        // String toUpdate = "0111161,The Shawshank Redemption,1,Drama,Too twoo Three long,Bob";
+        // FileIOObj.updateInFile("movie.csv", toUpdate);
+        // FileIOObj.deleteKeyInFile("movie.csv", "1375666");
+        FileIOObj.removeDuplicates("movie.csv");
     }
 }
