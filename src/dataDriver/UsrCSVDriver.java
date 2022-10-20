7626786ddbfcfedd60e2b07cb5f21f860f56a3b7
Tsien Jin <tsien.jin@gmail.com>
2022-10-20 12:39:23 +0800

    Added class names as per diagram

--- src/dataDriver/UsrCSVDriver.java
+++ src/dataDriver/UsrCSVDriver.java
@@ -5,6 +5,21 @@ public class UsrCSVDriver extends CSVDriver {
 
 
 
-    // METHOD to get specific 
+    // METHOD to get movie
+    public void getMovie(String id){
+
+    }
+
+    public void getBookingHistory(){
+
+    }
+
+    public void writeReview(){
+
+    }
+
+    public void bookSeat(){
+        
+    }
     
 }
