a564025189a3ff8241fe0b5322dce0d83328db59
rphoen <phoen.ryan@gmail.com>
2022-10-21 17:52:36 +0800

    Added moviebuilder, missing some methods

--- src/Movie/Movie.java
+++ src/Movie/Movie.java
@@ -3,35 +3,33 @@ package Movie;
 import java.util.ArrayList;
 
 public class Movie {
-    private int imdbID;
-    private String movieTitle;
-    enum showingStatus{Coming_Soon,Preview,Now_Showing};
-    private showingStatus status;
-    enum movieType{Blockbuster,ThreeD,Indie};
-    private movieType type;
-    private String synopsis;
-    private String director;
-    private ArrayList<String> cast;
-    private Double overallRating;
-    private ArrayList<Review> pastReviews;
-    private int totalTicketSales;
+    private final int imdbID;
+    private final String movieTitle;
+    private final showingStatus status;
+    private final movieType type;
+    private final String synopsis;
+    private final String director;
+    private final ArrayList<String> cast;
+    private final Double overallRating;
+    private final ArrayList<Review> pastReviews;
+    private final int totalTicketSales;
 
-    public Movie(int imdbID,String movieTitle,showingStatus status,movieType type,String synopsis,
-    String director,ArrayList<String> cast,Double overallRating,ArrayList<Review> pastReviews,int totalTicketSales)
+    public Movie(MovieBuilder builder)
     {
-        this.imdbID = imdbID;
-        this.movieTitle = movieTitle;
-        this.status = status;
-        this.type = type;
-        this.synopsis = synopsis;
-        this.director = director;
-        this.cast = cast;
-        this.overallRating = overallRating;
-        this.pastReviews = pastReviews;
-        this.totalTicketSales = totalTicketSales;
+        // can also add checks for null beforehand
+        this.imdbID = builder.imdbID;
+        this.movieTitle = builder.movieTitle;
+        this.status = builder.status;
+        this.type = builder.type;
+        this.synopsis = builder.synopsis;
+        this.director = builder.director;
+        this.cast = builder.cast;
+        this.overallRating = builder.overallRating;
+        this.pastReviews = builder.pastReviews;
+        this.totalTicketSales = builder.totalTicketSales;
     }
 
-    public Movie(String lineFromCSV){}
+    // public Movie(String lineFromCSV){}
 
     public int getImdbID(){
         return this.imdbID;
@@ -63,44 +61,4 @@ public class Movie {
     public int getTotalTicketSales(){
         return this.totalTicketSales;
     }
-
-    public void setImdbID(int imdbID){
-        this.imdbID = imdbID;
-    }
-    public void setMovieTitle(String movieTitle){
-        this.movieTitle = movieTitle;
-    }
-    public void setStatus(showingStatus status) {
-        this.status = status;
-    }
-    public void setMovieType(movieType type){
-        this.type = type;
-    }
-    public void setSynopsis(String synopsis){
-        this.synopsis = synopsis;
-    }
-    public void setDirector(String director){
-        this.director = director;
-    }
-    public void setOverallRating(Double overallRating){
-        this.overallRating = overallRating;
-    }
-    public void setTotalTicketSales(int totalTicketSales){
-        this.totalTicketSales = totalTicketSales;
-    }
-
-    // Either use the set or add method
-    public void setCast(ArrayList<String> cast){
-        this.cast = cast;
-    }
-    public void setPastReviews(ArrayList<Review> pastReviews){
-        this.pastReviews = pastReviews;
-    }
-    // public void addCast(String castMember){
-    //     this.cast.add(castMember);
-    // }
-    // public void addReview(String newReview){
-    //     this.pastReviews.add(newReview);
-    // }
-
 }
---
+++ src/Movie/MovieBuilder.java
@@ -0,0 +1,73 @@
+package Movie;
+import java.util.ArrayList;
+enum showingStatus{Coming_Soon,Preview,Now_Showing};
+enum movieType{Blockbuster,ThreeD,Indie};
+
+public class MovieBuilder {
+    int imdbID;
+    String movieTitle;
+    showingStatus status;
+    movieType type;
+    String synopsis;
+    String director;
+    ArrayList<String> cast;
+    Double overallRating;
+    ArrayList<Review> pastReviews;
+    int totalTicketSales;
+
+    public static MovieBuilder movie() {
+        return new MovieBuilder();
+    }
+
+    public MovieBuilder setImdbID(int imdbID){
+        this.imdbID = imdbID;
+        return this;
+    }
+    public MovieBuilder setMovieTitle(String movieTitle){
+        this.movieTitle = movieTitle;
+        return this;
+    }
+    public MovieBuilder setStatus(showingStatus status) {
+        this.status = status;
+        return this;
+    }
+    public MovieBuilder setMovieType(movieType type){
+        this.type = type;
+        return this;
+    }
+    public MovieBuilder setSynopsis(String synopsis){
+        this.synopsis = synopsis;
+        return this;
+    }
+    public MovieBuilder setDirector(String director){
+        this.director = director;
+        return this;
+    }
+    public MovieBuilder setOverallRating(Double overallRating){
+        this.overallRating = overallRating;
+        return this;
+    }
+    public MovieBuilder setTotalTicketSales(int totalTicketSales){
+        this.totalTicketSales = totalTicketSales;
+        return this;
+    }
+    public MovieBuilder setCast(ArrayList<String> cast){
+        this.cast = cast;
+        return this;
+    }
+    public MovieBuilder setPastReviews(ArrayList<Review> pastReviews){
+        this.pastReviews = pastReviews;
+        return this;
+    }
+    // public void addCast(String castMember){
+    //     this.cast.add(castMember);
+    // }
+    // public void addReview(String newReview){
+    //     this.pastReviews.add(newReview);
+    // }
+
+    public Movie build() {
+        return new Movie(this);
+    }
+
+}
