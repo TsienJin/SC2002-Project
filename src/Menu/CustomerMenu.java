ecb3b0aa8004075413e9d1214feec03f8b6e9abc
Tsien Jin <tsien.jin@gmail.com>
2022-10-28 14:29:16 +0800

    Removed commented out initial sections

--- src/Menu/CustomerMenu.java
+++ src/Menu/CustomerMenu.java
@@ -6,15 +6,6 @@ public class CustomerMenu extends MainMenu {
 
     @Override
     public void printMainMenu(){
-        // System.out.print("\nCustomer Menu:\n"
-        //                 + "01) List Movies\n"
-        //                 + "02) Search Movies\n"
-        //                 + "03) View Movie Details\n"
-        //                 + "04) Book ticket\n"
-        //                 + "05) View Booking History\n"
-        //                 + "06) List Top 5 Rankings\n"
-        //                 + "07) Quit\n");
-
         String header = "Customer Menu:";
         ArrayList<String> options = new ArrayList<>();
         options.add("List Movies");
--- src/Menu/StaffMenu.java
+++ src/Menu/StaffMenu.java
@@ -6,16 +6,6 @@ public class StaffMenu extends MainMenu {
 
     @Override
     public void printMainMenu(){
-        // System.out.print("\nStaff Menu:\n"
-        //                 + "01) Create Movie Listing\n"
-        //                 + "02) Update Movie Listing\n"
-        //                 + "03) Delete Movie Listing\n"
-        //                 + "04) Create Cinema Showtime\n"
-        //                 + "05) Update Cinema Showtime\n"
-        //                 + "06) Delete Cinema Showtime\n"
-        //                 + "07) Configure System Settings\n"
-        //                 + "08) Quit\n");
-
         String header = "Staff Menu:";
         ArrayList<String> options = new ArrayList<>();
         options.add("Create Movie Listing");
