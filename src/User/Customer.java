a68feaa70bcb63b4d4450d93764fc9216ec0d195
Tsien Jin <tsien.jin@gmail.com>
2022-10-29 21:40:20 +0800

    Added main running loop

--- src/User/Customer.java
+++ src/User/Customer.java
@@ -1,18 +1,53 @@
-public class Customer extends User{
-    
-    private int mobileNumber;
-    private int age;
+import Menu.CustomerMenu;
 
-    public Customer(int id, String name, String email, Role role, int mobileNumber, int age){
-        super(id, name, email, role);
-        this.mobileNumber = mobileNumber;
-        this.age = age;
+public class Customer extends User {
+    
+    public Customer(){
+        super.menu = new CustomerMenu();
     }
 
-    public void printInfo(){
-        super.printInfo();
-        System.out.printf("Mobile No.: \t%d\n" +
-                            "Age: \t%d\n", this.mobileNumber, this.age);
+    @Override
+    public void run(){
+        super.menu.printMainMenu();
+        int usrChoice = 0;
+        do{
+            usrChoice = super.getUsrChoice();
+            switch(usrChoice){
+                case 1:
+                    super.menu.printMainMenu();
+                    break;
+                case 2:
+                    // list all movies
+                    break;
+                case 3:
+                    // list top 5 movies by sales
+                    break;
+                case 4:
+                    // list top 5 movies by ratings
+                    break;
+                case 5:
+                    // search movie
+                    break;
+                case 6:
+                    // view movie details
+                    break;
+                case 7:
+                    // book ticket
+                    break;
+                case 8:
+                    // view booking history
+                    break;
+                case 9:
+                    // quit
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    usrChoice = 0;
+                    System.out.println("Invalid input! Select option 1 to print menu again.");
+                    break;
+            }
+        } while (usrChoice != 9);
+
     }
 
 }
