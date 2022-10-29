1b3238f92a24b1184a41636f67bc78673fd588ff
Baby-McBabyFace <muttlt25@gmail.com>
2022-10-29 21:06:20 +0800

    Updates

--- src/dataDriver/StaffCSVDriver.java
+++ src/dataDriver/StaffCSVDriver.java
@@ -1,16 +1,43 @@
 package dataDriver;
 
+import Movie.MovieBuilder;
+
 public class StaffCSVDriver extends CSVDriver {
  
     public boolean authenticate(String usrName, String usrPwd) {
+        FileIO fetchUserPass = new FileIO();
 
+        String userDetails = fetchUserPass.findMatchFromFile("staff.csv", usrName);
+        // String csvUsername = userDetails.
+        // String csvPassword = 
+        
+        if(1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public void createMovieListing() {
+        //Ask for input here?
+                
+        // after asking for all required parameters
+        String finalUserInput = "Hello world, goodbye world";
+
+        FileIO newMovie = new FileIO();
+        newMovie.writeToFile("movie.csv", finalUserInput);
 
     }
 
     public void updateMovieListing() {
+        //ask staff for movieID
+        String movieID = "123";
+        
+        String finalUserInput = "Hello world, goodbye world";
+        FileIO updateMovie = new FileIO();
+        updateMovie.overwriteToFile("movie.csv", finalUserInput);
+        
 
     }
 
