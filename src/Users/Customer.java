5ad3074566c497cc4c8d4024ab43b94e9b22d387
Tsien Jin <tsien.jin@gmail.com>
2022-11-01 22:31:01 +0800

    Changed dir for objects

---
+++ src/Users/Customer.java
@@ -0,0 +1,60 @@
+package Users;
+
+import Menu.CustomerMenu;
+import dataDriver.CustomerCSVDriver;
+
+public class Customer extends User {
+    
+    public Customer(){
+        super.menu = new CustomerMenu();
+        super.dataDriver = new CustomerCSVDriver();
+    }
+
+    @Override
+    public void run(){
+        super.menu.printMainMenu();
+        int usrChoice = 0;
+        do{
+            usrChoice = super.getUsrChoice();
+            switch(usrChoice){
+                case 1:
+                    super.menu.printMainMenu();
+                    break;
+                case 2:
+                    // list all movies
+                    super.dataDriver.listAllMovies();
+                    break;
+                case 3:
+                    // list top 5 movies by sales
+                    break;
+                case 4:
+                    // list top 5 movies by ratings
+                    break;
+                case 5:
+                    // search movie
+                    String rgx = input.getUsrString("Enter regex or string: ");
+                    super.dataDriver.listMovieFromRegex(rgx);
+                    break;
+                case 6:
+                    // view movie details
+                    break;
+                case 7:
+                    // book ticket
+                    break;
+                case 8:
+                    // view booking history
+                    break;
+                case 9:
+                    // quit
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    usrChoice = 0;
+                    System.out.println("Invalid input! Select option 1 to print menu again.");
+                    break;
+            }
+        } while (usrChoice != 9);
+
+    }
+
+}
