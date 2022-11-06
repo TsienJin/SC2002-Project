7c37c0e04e8ccd508fcbed64552f1eaf2d4f5b9d
keen <kelim1221@gmail.com>
2022-11-06 11:54:45 +0800

    bookingError solved

--- src/MovieTheatres/First.java
+++ src/MovieTheatres/First.java
@@ -23,13 +23,22 @@ public class First  {
     }
 
     public void initialLayout(){
-        int length = bookedSeat.length();
-        for(int i = 0; i < length; i=i+3){
-            int row = (bookedSeat.charAt(i))-'0';
-            int col = (bookedSeat.charAt(i+1))-'0';
-            layout[row][col] = 1;
+        if(this.bookedSeat.equals("null")){
+            for(int i = 0; i<size ;i++){
+                for(int j = 0; j<size ; j++){
+                    layout[i][j] = 0;
+                }
+            }
         }
+        else{
+            int length = bookedSeat.length();
+            for(int i = 0; i < length; i=i+3){
+                int row = (bookedSeat.charAt(i))-'0';
+                int col = (bookedSeat.charAt(i+1))-'0';
+                layout[row][col] = 1;
+            }
 
+        }
     }
 
     public void printInitialLayout(){
