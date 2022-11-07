7cb07858181ed74b1559e8d81c5406af8d3b61da
keen <kelim1221@gmail.com>
2022-11-08 00:43:42 +0800

    javadocs

--- src/Bookings/DifferentBooking.java
+++ src/Bookings/DifferentBooking.java
@@ -1,8 +1,15 @@
 package Bookings;
 
+/**
+ * Interface for different booking class
+ */
 public interface DifferentBooking {
 
     //abstract methods
+    /**
+     * Abstract class
+     * @return double booking price
+     */
     public double booking();
 
     
