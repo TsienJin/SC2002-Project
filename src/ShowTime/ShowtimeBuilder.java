32bb6374440a2e047c4d1a8f6ebcc5a369dff79e
Tsien Jin <tsien.jin@gmail.com>
2022-11-07 10:09:05 +0800

    added base javadoc

--- src/ShowTime/ShowtimeBuilder.java
+++ src/ShowTime/ShowtimeBuilder.java
@@ -22,6 +22,10 @@ public class ShowtimeBuilder implements InterfaceBuilder<showtime>, InterfaceCsv
     
     private FileIO fileio = new FileIO();
 
+    
+    /** 
+     * @return ShowtimeBuilder
+     */
     public static ShowtimeBuilder showtime(){
         return new ShowtimeBuilder();
     }
@@ -32,6 +36,10 @@ public class ShowtimeBuilder implements InterfaceBuilder<showtime>, InterfaceCsv
         this.updateFromCsvString(csvString);
     }
 
+    
+    /** 
+     * @param csvString
+     */
     private void updateFromCsvString(String csvString){
         ArrayList<String> csvArr = new ArrayList<>(Arrays.asList(csvString.split(mainDelimiter)));
 
@@ -44,6 +52,11 @@ public class ShowtimeBuilder implements InterfaceBuilder<showtime>, InterfaceCsv
 
     }
 
+    
+    /** 
+     * @param showtimeID
+     * @return ShowtimeBuilder
+     */
     public ShowtimeBuilder fromShowtimeID(String showtimeID){
         try {
             String csvString = fileio.findMatchFromFile(EnumDataFiles.Showtime.toString(), showtimeID);
@@ -54,26 +67,51 @@ public class ShowtimeBuilder implements InterfaceBuilder<showtime>, InterfaceCsv
         return this;
     }
 
+    
+    /** 
+     * @param showtimeID
+     * @return ShowtimeBuilder
+     */
     public ShowtimeBuilder setShowtimeID(String showtimeID){
         this.showtimeID = showtimeID;
         return this;
     }
 
+    
+    /** 
+     * @param movieId
+     * @return ShowtimeBuilder
+     */
     public ShowtimeBuilder setMovie(String movieId){
         this.movie = new MovieBuilder().fromMovieID(movieId).build();
         return this;
     }
 
+    
+    /** 
+     * @param cinemaCode
+     * @return ShowtimeBuilder
+     */
     public ShowtimeBuilder setCinema(String cinemaCode){
         this.cinema = new CinemaBuilder().fromCinemaCode(cinemaCode).build();
         return this;
     }
 
+    
+    /** 
+     * @param time_date
+     * @return ShowtimeBuilder
+     */
     public ShowtimeBuilder setTimeDate(String time_date){
         this.time_date = time_date;
         return this;
     }
 
+    
+    /** 
+     * @param cinemaCode
+     * @return String
+     */
     public String getCinemaClass(String cinemaCode){
         if(Character.compare(cinemaCode.charAt(0),'R')==0){
             return "Regular";
@@ -87,6 +125,10 @@ public class ShowtimeBuilder implements InterfaceBuilder<showtime>, InterfaceCsv
         return null;
     }
 
+    
+    /** 
+     * @return showtime
+     */
     @Override
     public showtime build(){
         return new showtime(this);
