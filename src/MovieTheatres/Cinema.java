8e5bfa3e850faa5674b169c17d6aae3e12fccf00
Baby-McBabyFace <muttlt25@gmail.com>
2022-11-07 15:59:50 +0800

    Added Javadoc

--- src/MovieTheatres/Cinema.java
+++ src/MovieTheatres/Cinema.java
@@ -9,6 +9,9 @@ public class Cinema implements InterfaceCsvDelimiter,InterfaceToCsvStringHelper{
     private int hallNumber;
     private String cinemaCode;
 
+    /** 
+     * Constructor for Cinema object
+     */
     public Cinema(CinemaBuilder builder){
         this.id = builder.id;
         this.hallNumber = builder.hallNumber;
@@ -16,6 +19,11 @@ public class Cinema implements InterfaceCsvDelimiter,InterfaceToCsvStringHelper{
     }
 
 
+    
+    /** 
+     * Returns a constructed string of attributes for Cinema object in the form of csv line
+     * @return Constructed string of attributes
+     */
     public String toCsvString() {
 
         // Delimiters are from the toCsvStringHelper interface
@@ -28,16 +36,36 @@ public class Cinema implements InterfaceCsvDelimiter,InterfaceToCsvStringHelper{
         return String.join(mainDelimiter, arrCSV);
     }
 
+    
+    /** 
+     * Returns attribute ID of Cinema object
+     * @return ID attribute
+     */
     //Accessor Methods
     public String getId(){
         return this.id;
     }
+    
+    /** 
+     * Returns attribute hallNumber of Cinema object
+     * @return hallNumber attribute
+     */
     public int getHallNumber(){
         return this.hallNumber;
     }
+    
+    /** 
+     * Returns attribute cinemaCode of Cinema object
+     * @return cinemaCode attribute
+     */
     public String getCinemaCode(){
         return this.cinemaCode;
     }
+    
+    /** 
+     * Returns attribute cinemaClass of Cinema object from cinemaCode
+     * @return cinemaClass attribute
+     */
     public String getCinemaclass(){
         char firstchar = this.cinemaCode.charAt(0);
         if(Character.compare(firstchar,'R')==0){
@@ -56,12 +84,27 @@ public class Cinema implements InterfaceCsvDelimiter,InterfaceToCsvStringHelper{
     }
 
     //Mutator Methods
+    
+    /** 
+     * Setter for attribute ID for Cinema object
+     * @param id
+     */
     public void setId(String id){
         this.id = id;
     }
+    
+    /** 
+     * Setter for attribute hallNumber for Cinema object
+     * @param hallNumber
+     */
     public void setHallNumber(int hallNumber){
         this.hallNumber = hallNumber;
     }
+    
+    /** 
+     * Setter for attribute cinemaCode for Cinema object
+     * @param cinemaCode
+     */
     public void setCinemaCode(String cinemaCode){
         this.cinemaCode = cinemaCode;
     }
